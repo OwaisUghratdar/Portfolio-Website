fff297dd990cdaaafc5d9139c9c80e14617698de
Owais Ughratdar <owais.ughratdar@gmail.com>
2023-08-11 19:17:47 -0400

    initial commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { data } from "../data/data.js";
+import WorkTable from "./WorkTable.jsx";
+
+
+const Experience = () => {
+
+    
+
+    // projects file
+    const project = data;
+    //setProject(data);
+  
+  return (
+    
+    <div id='experience-id' name='experience' className='pt-48 w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p name='experience-title' className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+          Experience
+          </p>
+          <p className='py-6'>Take a look at my recent experience:</p>
+        </div>
+
+        <WorkTable />
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import CarProjectBG from '../assets/car_project_bg.jpg'
+import CarBG from '../assets/car-bg.png'
+import Whatsapp from '../assets//whatsapp.jpg'
+import Shoppies from '../assets/shoppies.JPG'
+import Ums from '../assets//ums.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Vehicle Showcase React Webapp",
+        number:"1",
+        image:CarBG,
+        github:"https://github.com/OwaisUghratdar/Vehicles-Showcase-Site",
+        live:"https://vehicles-showcase-site-git-main-owaisughratdar.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Whatsapp Clone React Webapp",
+        number:"2",
+        image:Whatsapp,
+        github:"",
+        live:"https://whatsapp-clone-reactjs-318c3.web.app/",
+    },
+    {
+        id:3,
+        name:"The Shoppies Movie Review Application",
+        number:"3",
+        image:Shoppies,
+        github:"",
+        live:"https://theshoppies-7b17c.web.app/",
+    },
+    {
+        id:4,
+        name:"User Management System Webapp",
+        number:"4",
+        image:Ums,
+        github:"https://github.com/OwaisUghratdar/User-Management-System",
+        live:"https://user-management-system-dghj-git-master-owaisughratdar.vercel.app/",
+    },
+
+
+]
